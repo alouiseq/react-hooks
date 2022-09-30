f9b4f548d55fde7fffbbc462cb3e47278d6d4e56
Louie <alouiseq@gmail.com>
2022-09-29 20:58:40 -0700

    complete ex1 ec

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,8 +3,8 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  const [name, setName] = React.useState('')
+function Greeting({ initName }) {
+  const [name, setName] = React.useState(initName)
 
   function handleChange(event) {
     setName(event.target.value)
@@ -22,7 +22,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initName="Bobby"/>
 }
 
 export default App
